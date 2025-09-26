83fc5b1c6176379a0739ee8ba1c6448029f2caee
yash <ys0979727@gmail.com>
2025-09-26 21:03:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Route for /hello
+app.get("/", (req, res) => {
+  res.send("Hellodd from Node.js!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
