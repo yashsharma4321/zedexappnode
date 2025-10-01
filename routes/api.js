ec340004e2db59892d5daf4986fefef68dbfec96
yash <ys0979727@gmail.com>
2025-10-01 21:38:08 +0530

    hello

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 import express from 'express';
-// import { getAllUsers, createUser } from '../controllers/USER/usercontroller.js';
+import { getAllUsers, createUser } from '../controllers/USER/usercontroller.js';
 
 
 const router = express.Router();
 
+router.get('/users', getAllUsers);
+router.post('/users', createUser);
 
 router.get('/', (req, res) => {
   // Render the index.ejs from the views folder with optional data
